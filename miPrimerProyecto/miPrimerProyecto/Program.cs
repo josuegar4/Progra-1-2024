af4b671c93bb73da74d1bc6ac6e1eda9677110c9
JOSUÉ ELÍAS GARCÍA LÓPEZ <USSS046123@ugb.edu.sv>
2024-09-04 17:06:04 -0600

    Suma de dos numeros ingresados por el usuario

---
+++ miPrimerProyecto/miPrimerProyecto/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miPrimerProyecto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("num1:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("num2:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int respuesta = num1 + num2;
+            Console.WriteLine("La suma de {0} +  {1 } =  {2}", num1, num2, respuesta);
+
+            //PAUSA
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
