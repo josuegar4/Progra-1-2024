43f1f71f0becbb57747ab58cedf3dbb2677f3a4a
JOSUÉ ELÍAS GARCÍA LÓPEZ <USSS046123@ugb.edu.sv>
2024-09-04 17:12:16 -0600

    Tipos de datos en C#

--- miPrimerProyecto/miPrimerProyecto/Program.cs
+++ miPrimerProyecto/miPrimerProyecto/Program.cs
@@ -10,16 +10,18 @@ namespace miPrimerProyecto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("num1:");
-            sbyte num1 = sbyte.Parse(Console.ReadLine());
+            Boolean comprobar = true;
+            Console.WriteLine("El valor es: {0}", comprobar);
 
-            Console.WriteLine("num2:");
-            sbyte num2 = sbyte .Parse(Console.ReadLine());
+            Console.Write("Nombre: ");
+            string nombre = Console.ReadLine();
 
-            int suma = num1 + num2;
-            Console.WriteLine("La suma de {0} +  {1 } =  {2}", num1, num2, suma);
+            Console.WriteLine("Hola {0} bienvenido", nombre);
 
-            //PAUSA
+            DateTime fechaActual = new DateTime();
+            fechaActual = DateTime.Now;
+            Console.WriteLine("La fecha actual es: {0}", fechaActual);
+            //Pausa.
             Console.ReadLine();
 
 
