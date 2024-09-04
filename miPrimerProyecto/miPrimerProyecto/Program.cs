969b97f08e178ab4c807b2a258c1bd118022334a
JOSUÉ ELÍAS GARCÍA LÓPEZ <USSS046123@ugb.edu.sv>
2024-09-04 17:22:40 -0600

    NOta final de Computo 1 y 2 + tarea

--- miPrimerProyecto/miPrimerProyecto/Program.cs
+++ miPrimerProyecto/miPrimerProyecto/Program.cs
@@ -14,13 +14,10 @@ namespace miPrimerProyecto
             //ejercicio de obtener la nota final de la materia de Programacion I.
             Console.Write("Lab1: ");
             double lab1 = double.Parse(Console.ReadLine()); //8
-
             Console.Write("Lab2: ");
             double lab2 = double.Parse(Console.ReadLine()); //9
-
             Console.Write("Parcial 1: ");
             double parcial1 = double.Parse(Console.ReadLine()); //7
-
             // 8*30%= 2.4
             // 9*30%= 2.7
             // 7*40%= 2.8
@@ -28,9 +25,24 @@ namespace miPrimerProyecto
 
             double c1 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
             Console.WriteLine("La nota de C1 es: {0}", c1);
+
+            Console.Write("Lab1: ");
+            lab1 = double.Parse(Console.ReadLine()); //8
+
+            Console.Write("Lab2: ");
+            lab2 = double.Parse(Console.ReadLine()); //9
+
+            Console.Write("Parcial 1: ");
+            parcial1 = double.Parse(Console.ReadLine()); //7
+
+            double c2 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
+            Console.WriteLine("La nota de C2 es: {0}", c2);
+
+            //TAREA: Obtener la nota final por computo y Nota Final de la matera de Programacion I.
+
+
             //Pausa.
             Console.ReadLine();
-
 
 
         }
