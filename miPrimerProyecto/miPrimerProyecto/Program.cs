1de02b022afc5c3abf372636ab6bec4adebfac9c
JOSUÉ ELÍAS GARCÍA LÓPEZ <USSS046123@ugb.edu.sv>
2024-09-04 18:29:32 -0600

    Creacion de una vector

--- miPrimerProyecto/miPrimerProyecto/Program.cs
+++ miPrimerProyecto/miPrimerProyecto/Program.cs
@@ -10,53 +10,11 @@ namespace miPrimerProyecto
     {
         static void Main(string[] args)
         {
-            //2. Switch ejercicio. pedir al usuario la edad si es mayor de edad que le diga bienvenido.
-
-            string continuar = "s";
-            while (continuar == "s")
-            {
-                Console.WriteLine("\n\n *** MENU ***");
-                Console.WriteLine("1. Promedio Notas");
-                Console.WriteLine("2. Promedio Serie Numeros");
-                Console.WriteLine("3. Clasificacion edad");
-                Console.WriteLine("4. Tabla Multiplicar");
-                Console.WriteLine("5. Salir");
-                Console.Write("Opcion: ");
-                int opcion = int.Parse(Console.ReadLine());
-                Console.Clear();//limpiar la consola
-                switch (opcion) {
-                    case 1://if(opcion==1)
-                        promedio();
-
-                    case 3: //if(opcion==3)
-                        ClasificacionEdad();
-                        break;
-                    case 4:
-                        tablaMultiplicar();
-                        break;
-                    case 5: //if(opcion==5)
-                        continuar = "n";
-                        break;
-                    default://else
-
-
-                }
-            }
-        }
-
-        static void tablaMultiplicar()
-        {
-            Console.Write("Tabla: ");
-            int ntabla = int.Parse(Console.ReadLine());
-            for (int i = 1; i <= 10; i++)
-            {//i++ => i = i + 1
-                Console.WriteLine("{0}x{1}={2}", ntabla, i, ntabla * i);
-            }
-        }
-        static void promedio()
-        {
-            Console.Write("Lab1: ");
-            double lab1 = double.Parse(Console.ReadLine());
+            //Uso de matrices.
+            int[] numeros = new int[] { 2, 8, 9, 10, 1, 3 };
+            Console.WriteLine("El numero de la posicion 3 es {0}", numeros[3]);
+            //Pausa
+            Console.ReadKey();
         }
     }
 }
