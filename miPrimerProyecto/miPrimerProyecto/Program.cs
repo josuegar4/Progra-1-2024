295165cefacf7cb82efebfa22fe6dcbd70182b7d
JOSUÉ ELÍAS GARCÍA LÓPEZ <USSS046123@ugb.edu.sv>
2024-09-04 19:35:26 -0600

    Uso de funciones con parametros

--- miPrimerProyecto/miPrimerProyecto/Program.cs
+++ miPrimerProyecto/miPrimerProyecto/Program.cs
@@ -10,9 +10,16 @@ namespace miPrimerProyecto
     {
         static void Main(string[] args)
         {
-
             //Uso de funciones...
             Console.WriteLine("La suma de 10+5={0}", suma());
+
+            Console.Write("Num 1: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Num 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("La suma de {0}+{1}={2}", num1, num2, suma(num1, num2));
 
             Console.ReadLine();
         }
@@ -20,6 +27,12 @@ namespace miPrimerProyecto
         {
             int respuesta = 10 + 5;
             return respuesta;
+        }
+        static int suma(int a, int b)
+        {
+            int respuesta = a + b;
+            return respuesta;
+
         }
 
     }
